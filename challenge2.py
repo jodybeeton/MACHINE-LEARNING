96adc77dbd164b42f435d3a1afa6e8ba547ee88b
jody <misscosmo43@gmail.com>
2020-11-04 14:59:05 +0200

    Histogram

---
+++ challenge2.py
@@ -0,0 +1,1 @@
+import matplotlib.pyplot as pltimport numpy as npnums = np.array([0.5, 0.7, 1.0, 1.2, 1.3, 2.1])bins = np.array([0, 1, 2, 3])plt.hist(nums, bins, color='indigo')# CREATING THE X LABEL NAMES NUMSplt.xlabel("Nums")# CREATING THE Y LABEL NAMED BINSplt.ylabel("Bins")plt.title("HISTOGRAM OF NUMS AGAINST BINS")plt.show()
